7877ee21e3d5c50348b11263567f59c18a686f18
jeffpsrb <jeffreyp454r1bu@gmail.com>
2023-08-23 10:25:22 +0700

    Update

--- src/Materi/Form/index.jsx
+++ src/Materi/Form/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 // import FormElemen from "./pembahasan/formElemen";
-import Validation from "./pembahasan/validation";
+// import Validation from "./pembahasan/validation";
+import Validations from "./pembahasan/validator";
 
 export default class Form extends React.Component {
     render(){
         return(
-            <Validation />
+            <Validations />
         )
     }
 }
