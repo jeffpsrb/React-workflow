dad9783a922e56d3eb2cb673cf197fdc47bbdc1b
jeffpsrb <jeffreyp454r1bu@gmail.com>
2023-08-29 15:38:31 +0700

    Update Commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 // import Bootstrap from './Materi/Styling/Bootstrap';
 // import Reausable from './Materi/Reusable/hello';
-import Rendering from './Materi/Rendering';
+// import Rendering from './Materi/Rendering';
 import Form from './Materi/Form';
+import Lifecycle from './Materi/Lifecycle';
 
 
 function App() {
   return (
     <div className='App'>
-      <Form />
+      <Lifecycle />
 
     </div>
   );
